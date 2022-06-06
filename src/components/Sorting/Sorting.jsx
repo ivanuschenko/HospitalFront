836cdae8d4affbf580ffebe3024f1e90b9ec69df
Your Name <you@example.com>
2022-06-06 23:54:35 +0300

    fix bugs with name of components

--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import addSort from 'src/img/plus.png';
 import { fields } from 'src/constants';
 import './style.scss';
 
 const Sorting = ({list, setList}) => { 
     
-  const [sortField, setSortField] = useState('');
-  const [opensortField, setOpensortField] = useState(false); 
-  const [sortWay, setSortWay] = useState('asc');
+  const [sortingField, setSortingField] = useState('');
+  const [openSortDirection, setOpenSortDirection] = useState(false); 
+  const [sortingWay, setSortingWay] = useState('asc');
 
   const sort = () => {    
     const sortedList = [...list].sort((a, b) => (    
-      (a[sortField] > b[sortField]) ? -1 : ((a[sortField] < b[sortField]) ? 1 : 0))  
+      (a[sortingField] > b[sortingField]) ? -1 : ((a[sortingField] < b[sortingField]) ? 1 : 0))  
     );
-    if (sortWay === 'desc') sortedList.reverse();         
+    if (sortingWay === 'desc') sortedList.reverse();         
     setList(sortedList);    
   } 
 
   useEffect( () => {         
     sort();     
-  },[sortField, sortWay]); 
+  }, [sortingField, sortingWay]); 
 
   return (
     <div className='sorting'>    
       <div className='sorting-body'>
         <div className='sorting-block'>
-          <label className='sorting-block_label__byfield' htmlFor="sorting-block_select__field">Сортировать по:</label>
+          <label className='sorting-block_label__field' htmlFor='sorting-block_select__field'>Сортировать по:</label>
           <select 
-            className="sorting-block_select__field"
-            onClick= {() => setOpensortField(true)}
-            onChange={(e) => setSortField(e.target.value)}
+            className='sorting-block_select__field'
+            onClick={() => setOpenSortDirection(true)}
+            onChange={(e) => setSortingField(e.target.value)}
           >
             <option></option>          
             {
@@ -45,27 +45,27 @@ const Sorting = ({list, setList}) => {
             }
           </select>
         </div>        
-        <div className={opensortField ? 'sorting-block__direction active' : 'sorting-block__direction '} >
-          <label htmlFor="sorting-select__direction">Направление:</label>
+        <div className={openSortDirection ? 'sorting-block__direction active' : 'sorting-block__direction '} >
+          <label htmlFor='sorting-select__direction'>Направление:</label>
           <select 
-            className="sorting-block_select__direction" 
-            onChange={(e)=> setSortWay(e.target.value)}>
+            className='sorting-block_select__direction' 
+            onChange={(e) => setSortingWay(e.target.value)}>
               <option></option>
-              <option className='sorting-block_option__direction' value="asc" >По возрастанию</option>
-              <option className='sorting-block_option__direction' value="desc">По убыванию</option>
+              <option className='sorting-block_option__direction' value='asc' >По возрастанию</option>
+              <option className='sorting-block_option__direction' value='desc'>По убыванию</option>
           </select>
         </div>
         <div className='sorting-block'>
-          <label htmlFor="sorting-block_button__open-filter">Добавить фильтр по дате:</label>
+          <label htmlFor='sorting-block_button__open-filter'>Добавить фильтр по дате:</label>
           <button 
             type='button' 
             id='sorting-block_button__open-filter' 
             className='sorting-block_button__open-filter'
           >
             <img src={addSort} 
-              alt="btn-add-filter" 
-              className='sorting-blokc_img__addFilter' 
-              onClick={()=>alert(true)} 
+              alt='btn-add-filter' 
+              className='sorting-block_img__addFilter' 
+              onClick={() => alert('future logic for open filter components')}  //future logic for open filter components 
             />
           </button>          
         </div>                
